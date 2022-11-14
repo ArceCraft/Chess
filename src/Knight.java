8d0b98904acd656ef8630365a0e6f589bc8f3e7f
ArceCraft <113393688+ArceCraft@users.noreply.github.com>
2022-11-14 12:20:42 -0700

    Update Knight.java

--- src/Knight.java
+++ src/Knight.java
@@ -40,7 +40,32 @@ public class Knight extends Piece{
 
     @Override
     public Square[] ProtectingMoves() {
-        return new Square[0];
+        List<Square> freeMoves = new ArrayList<>();
+
+
+        int pivotRow = getPlaceAt().getRow();
+        int pivotColum = getPlaceAt().getColumn();
+
+        for (int[] moveDirection : moveDirections) {
+
+            for (int i = 1; i < 2; i++) {
+                if(pivotRow + moveDirection[0]*i < 0 || pivotRow + moveDirection[0]*i >= 8 || pivotColum + moveDirection[1]*i < 0 || pivotColum + moveDirection[1]*i >= 8)
+                    break;
+                else if(CaptureMoveValidation(new Square(pivotRow + moveDirection[0]*i, pivotColum + moveDirection[1]*i)))
+                    break;
+                else if (FreeMoveValidation(new Square(pivotRow + moveDirection[0]*i, pivotColum + moveDirection[1]*i))) {
+                    freeMoves.add(new Square(pivotRow + moveDirection[0]*i, pivotColum + moveDirection[1]*i));
+                    break;
+                } else
+                    freeMoves.add(new Square(pivotRow + moveDirection[0]*i, pivotColum + moveDirection[1]*i));
+            }
+
+        }
+
+
+        Square[] freeMovesArray = new Square[freeMoves.size()];
+
+        return freeMoves.toArray(freeMovesArray);
     }
 
     @Override
